f593f7b123a1a370606a8dee887036aa5ecc6615
Saeed1112 <nullee.sa@gmail.com>
2023-10-27 20:18:37 +0330

    header

--- app/page.tsx
+++ app/page.tsx
@@ -12,15 +12,13 @@ export default function Home() {
           <div
             onClick={() => setActive(item)}
             key={item}
-            className={`px-5 cursor-pointer font-light relative rounded-full h-10 flex items-center justify-center ${
-              active === item ? "" : ""
-            } `}
+            className="px-5 cursor-pointer font-light relative rounded-full h-10 flex items-center justify-center hover:opacity-50 transition-opacity duration-300"
           >
             {active === item && (
               <motion.div
-                className="absolute inset-0 bg-gray-50 shadow-[0_0_49px_-15px]"
+                className="absolute inset-0 bg-gray-50 shadow-[0_0_50px_-15px]"
                 layoutId="root"
-                style={{ borderRadius: 999 }}
+                style={{ borderRadius: 50 }}
                 transition={{ duration: 0.7, type: "spring" }}
               ></motion.div>
             )}
