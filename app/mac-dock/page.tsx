cd80c124e1205d931ab52e06ee1200a7cc4ac6f0
Saeed1112 <nullee.sa@gmail.com>
2023-10-28 16:01:10 +0330

    title

--- app/mac-dock/page.tsx
+++ app/mac-dock/page.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { AnimatedMacDock } from "@/app/mac-dock/animated-mac-dock";
-
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Mac Dock",
+};
 const Page = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
